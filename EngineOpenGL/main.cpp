722f48536aff9cded5b1fe4a1a7935a2acb932c0
CasperStar <Casper@mbp-van-casper.home>
2017-02-08 17:23:00 +0100

    Initial commit

--- EngineOpenGL/main.cpp
+++ EngineOpenGL/main.cpp
@@ -1,15 +1,9 @@
-//
-//  main.cpp
-//  EngineOpenGL
-//
-//  Created by Casper Star on 08-02-17.
-//  Copyright © 2017 Casper Star. All rights reserved.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
+int main()
+{
     std::cout << "Hello, World!\n";
+    
     return 0;
 }
+ß
