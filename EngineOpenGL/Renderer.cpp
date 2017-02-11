18ebd047c555733154cbe19b15e2e7d97f818ed6
CasperStar <Casper@mbp-van-casper.home>
2017-02-11 17:15:02 +0100

    Log OpenGL version

--- EngineOpenGL/Renderer.cpp
+++ EngineOpenGL/Renderer.cpp
@@ -8,6 +8,12 @@ Renderer::Renderer()
         Logger::GetInstance()->Log("Renderer: Failed to init GLEW", LOG_ERROR);
     }
     
+    // Check OpenGL Version
+    std::stringstream stringStream;
+    stringStream << "Renderer: OpenGL Version: " << glGetString(GL_VERSION);
+    Logger::GetInstance()->Log(stringStream.str(), LOG_INFO);
+
+    
     // Create ShaderManager
     m_pShaderManager = new ShaderManager();
 
